86ed08fc1c23853abc5ad72dce01787f79b68b83
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2024-01-03 22:01:01 +0530

    03-01-2023

---
+++ 11.dart
@@ -0,0 +1,18 @@
+
+import 'dart:io';
+
+void main()
+{
+  int n = 20;
+
+  stdout.write("enter the nummber of n ");
+  n=int.parse(stdin.readLineSync()!);
+
+  for (int i=1;i<=15;i++)
+  {
+    n = += i;
+     if(i == 1);
+     print("$n");
+
+  }
+}
