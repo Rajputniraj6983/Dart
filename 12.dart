ceadd0c1757dd0afb92e59d642ac4658c60e0333
R@JPUT NIR@J <143181391+Rajputniraj6983@users.noreply.github.com>
2024-01-04 16:46:50 +0530

    04-01-2023

---
+++ 12.dart
@@ -0,0 +1,39 @@
+
+
+import 'dart:io';
+
+void main()
+
+{
+  
+ //   var list = [1,2,3,4,5];
+ //   int n;
+ //   stdout.write("enter the number ");
+//    n=int.parse(stdin.readLineSync()!);
+
+ // list.add(n);
+ // print(list);
+
+
+
+ // var list = [1,2,3,4,5];
+ // int n;
+ // stdout.write("enter the number ");
+ // n=int.parse(stdin.readLineSync()!);
+
+ // list.removeAt(n);
+ // print(list);
+
+
+        var list =[1,2,3,4,5,6];
+        int n;
+
+        stdout.write("enter the number ");
+        n=int.parse(stdin.readLineSync()!);
+
+        list.remove(3);
+        print(list);
+
+
+
+}
